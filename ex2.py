7107d340ba2609a2ffa165e8a7517279f1f12e4a
jimroma <jim@jimroma.com>
2014-01-11 15:23:31 -0500

    ex2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# a comment - read at your own risk
+# ;alsdl;fjalsjdfla;lkdjfijealdjffjiefa
+print "I printed something"; # here's a commment
+print "pound hashtag  # octothrope";
+
+
+############ blah
+print "blah";
