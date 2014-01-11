a7108dba5a4ef4f51b9bf5fcd17eeac1d47b2605
jimroma <jim@jimroma.com>
2014-01-11 15:42:38 -0500

    ex3

---
+++ ex3.py
@@ -0,0 +1,19 @@
+print "I will now conunt my chickens";
+print "hens", 25 + 30 /6;
+
+print "Now, I'll count the eggs:"
+# use floating point numbers
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "more horseshit"
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it greater less or equal", 5 <= -2
