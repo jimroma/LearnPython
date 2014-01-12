55afcbac91a0dd9aaf41d449b8f4f2df074d1655
jimroma <jim@jimroma.com>
2014-01-11 21:04:29 -0500

    ex09

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #string with new line characters in it
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# triple quotes allow you to out put formatted text
+print """
+There's something here 
+but you don't know what it is
+do you? Mr. Jones.
+i   like   turtles   zebra
+"""
